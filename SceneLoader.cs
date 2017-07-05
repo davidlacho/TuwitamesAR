b9f41eab627be2e3069473b9582584142b3b5e13
David Lacho <david.lacho@gmail.com>
2017-07-05 16:37:22 -0700

    Added silly messages
    Added holder for silly messages that load on screen

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour {
 
 	private bool sceneLoaded = false;
+	private bool scenesFinishedLoaded = false; 
 	[Header ("Scene Names (Don't Add Scene To Load To)")]
 	[SerializeField]
 	private string[] sceneNames;
@@ -15,6 +16,10 @@ public class SceneLoader : MonoBehaviour {
 	private string sceneToLoadTo;
 	[SerializeField]
 	private Text loadingMessage;
+	[SerializeField]
+	private Text sillyText;
+	[SerializeField]
+	private string[] sillyTextMessages;
 
 	//For Loading Bar:
 	[SerializeField] 
@@ -31,6 +36,7 @@ public class SceneLoader : MonoBehaviour {
 			sceneLoaded = true;
 			loadingMessage.text = " ...";
 			StartCoroutine (LoadScene ());
+			StartCoroutine(ChangeSillyText());
 		}
 		if (sceneLoaded == true) {
 			loadingMessage.color = new Color (loadingMessage.color.r, loadingMessage.color.g, loadingMessage.color.b, Mathf.PingPong (Time.time, 1));
@@ -55,6 +61,7 @@ public class SceneLoader : MonoBehaviour {
 		}
 		progressBarLoaded++;
 
+
 		//Load Each Scene:
 		foreach (string scene in sceneNames) {
 			AsyncOperation sceneLoader = SceneManager.LoadSceneAsync (scene, LoadSceneMode.Additive);
@@ -62,6 +69,7 @@ public class SceneLoader : MonoBehaviour {
 				yield return null;
 			}
 			progressBarLoaded++;
+
 		}
 
 		//Kill Each Scene
@@ -72,12 +80,24 @@ public class SceneLoader : MonoBehaviour {
 			}
 			progressBarLoaded++;
 			sceneCounter--;
+
 		}
 
 		//Load Main Scene:
+		scenesFinishedLoaded = true; 
 		AsyncOperation async = SceneManager.LoadSceneAsync (sceneToLoadTo, LoadSceneMode.Single);
 		while (!async.isDone) {
 			yield return null;
+		}
+	}
+
+	IEnumerator ChangeSillyText () {
+		while (!scenesFinishedLoaded) {
+			int randomNumber = Random.Range (0, (sillyTextMessages.Length));
+			Debug.Log(randomNumber);
+			sillyText.text = sillyTextMessages [randomNumber];
+			yield return new WaitForSeconds (2);
+
 		}
 	}
 }
