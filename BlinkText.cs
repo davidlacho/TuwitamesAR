5ae76de7b013426d8291fdf3c8b8ba5e4bb07fe7
David Lacho <david.lacho@gmail.com>
2017-07-12 14:49:19 -0500

    Initialization
    Loading screen is now for initialization. During initialization
    individual scenes with crucial game components load. These components
    have DoNotDestroyOnLoad script attached to them, so when scenes change
    they will stay constant in the game. While scene is loading
    (Coroutine), loading message now shows to give idea that there’s
    something going on to the user.

---
+++ BlinkText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour {
+
+	Text text;
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.color = new Color (text.color.r, text.color.g, text.color.b, Mathf.PingPong (Time.time, 1));
+	}
+}
--- SceneLoader.cs
+++ SceneLoader.cs
@@ -7,12 +7,12 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour {
 
 	private bool sceneLoaded = false;
-	private bool scenesFinishedLoaded = false; 
-	[Header ("Scene Names (Don't Add Scene To Load To)")]
+	private bool scenesFinishedLoaded = false;
+	[Header ("Initialization Scene To Load")]
 	[SerializeField]
-	private string[] sceneNames;
-	[Header ("Scene To Load To")]
-	[SerializeField] 
+	private string[] scenesToLoad;
+	[Header ("Scene To Load To After Initialization")]
+	[SerializeField]
 	private string sceneToLoadTo;
 	[SerializeField]
 	private Text loadingMessage;
@@ -20,6 +20,7 @@ public class SceneLoader : MonoBehaviour {
 	private Text sillyText;
 	[SerializeField]
 	private string[] sillyTextMessages;
+
 
 	//For Loading Bar:
 	[SerializeField] 
@@ -29,6 +30,7 @@ public class SceneLoader : MonoBehaviour {
 
 	void Start () {
 		progressSlider.value = 0f;
+		progressBarTotal = scenesToLoad.Length + 1;
 	}
 
 	void Update () {
@@ -36,7 +38,7 @@ public class SceneLoader : MonoBehaviour {
 			sceneLoaded = true;
 			loadingMessage.text = " ...";
 			StartCoroutine (LoadScene ());
-			StartCoroutine(ChangeSillyText());
+			StartCoroutine (ChangeSillyText ());
 		}
 		if (sceneLoaded == true) {
 			loadingMessage.color = new Color (loadingMessage.color.r, loadingMessage.color.g, loadingMessage.color.b, Mathf.PingPong (Time.time, 1));
@@ -45,46 +47,17 @@ public class SceneLoader : MonoBehaviour {
 	}
 
 	IEnumerator LoadScene () {
-
-		int sceneCounter = 0;
-		foreach (string scene in sceneNames) {
-			sceneCounter++;
-			progressBarTotal = progressBarTotal + 2; 
-		}
-
-		yield return new WaitForSeconds (2);
-
-		//Load Main Scene:
-		AsyncOperation loadMainScene = SceneManager.LoadSceneAsync (sceneToLoadTo, LoadSceneMode.Additive);
-		while (!loadMainScene.isDone) {
-			yield return null;
-		}
-		progressBarLoaded++;
-
-
-		//Load Each Scene:
-		foreach (string scene in sceneNames) {
+		foreach (string scene in scenesToLoad) {
 			AsyncOperation sceneLoader = SceneManager.LoadSceneAsync (scene, LoadSceneMode.Additive);
 			while (!sceneLoader.isDone) {
 				yield return null;
 			}
 			progressBarLoaded++;
-
 		}
 
-		//Kill Each Scene
-		while (sceneCounter > 0) {
-			AsyncOperation killScene = SceneManager.UnloadSceneAsync (sceneNames [sceneCounter - 1]);
-			while (!killScene.isDone) {
-				yield return null;
-			}
-			progressBarLoaded++;
-			sceneCounter--;
-
-		}
-
-		//Load Main Scene:
+		//Load After Initialization:
 		scenesFinishedLoaded = true; 
+		progressBarLoaded++;
 		AsyncOperation async = SceneManager.LoadSceneAsync (sceneToLoadTo, LoadSceneMode.Single);
 		while (!async.isDone) {
 			yield return null;
@@ -94,7 +67,6 @@ public class SceneLoader : MonoBehaviour {
 	IEnumerator ChangeSillyText () {
 		while (!scenesFinishedLoaded) {
 			int randomNumber = Random.Range (0, (sillyTextMessages.Length));
-			Debug.Log(randomNumber);
 			sillyText.text = sillyTextMessages [randomNumber];
 			yield return new WaitForSeconds (2);
 
