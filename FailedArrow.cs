0f39a736efe5e74a66701d17eead57e995c6a341
David Lacho <david.lacho@gmail.com>
2017-07-28 12:29:20 -0500

    Hunter Arrow Actions
    These scripts are for the hunter. There are some animation events that
    trigger gameobjects to be enabled/disabled. This creates the effect of
    the hunter grabbing his bow, reaching into his quiver for an arrow,
    moving the arrow on to the bow (switching the arrow’s hand), pulling
    back, and then an arrow is fired that "fails" as the hunter cannot
    reach the deer.

---
+++ FailedArrow.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailedArrow : MonoBehaviour {
+
+	public Transform[] waypoint;
+	private Transform currentWaypointGoal;
+	private Vector3 currentPosition;
+	private Vector3 destinationToFlyTo;
+
+	private int waypointCounter = 0;
+
+	private bool reachedDestination;
+
+	public float speed;
+	private float trueSpeed;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		trueSpeed = speed * Time.deltaTime;
+		currentPosition = transform.position;
+		currentWaypointGoal = waypoint [waypointCounter]; 
+		Vector3 destinationToFlyTo = currentWaypointGoal.position; 
+
+		float distanceToTarget = calculateDistanceToTarget (currentWaypointGoal);
+
+
+		Flying (destinationToFlyTo, currentWaypointGoal); 
+
+		if (distanceToTarget > 0.5f) {
+					reachedDestination = false;
+				} else {
+					reachedDestination = true; 
+				}
+
+
+		if (reachedDestination && waypointCounter < (waypoint.Length - 1)) {
+			waypointCounter++;
+			reachedDestination = false; 
+		} 
+
+		if (reachedDestination && waypointCounter == (waypoint.Length - 1)) {
+			gameObject.SetActive(false);
+		}
+
+	}
+
+
+	private float calculateDistanceToTarget (Transform destination) {
+		float distanceToTarget = Vector3.Distance (currentPosition, destination.position); 
+		return distanceToTarget;
+	}
+
+	private void Flying (Vector3 waypoint, Transform currentWaypointGoal) {
+
+		Vector3 targetDir = currentWaypointGoal.position - transform.position;
+		Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, trueSpeed, 0);
+		transform.rotation = Quaternion.LookRotation (newDir);
+		transform.position = Vector3.MoveTowards (currentPosition, waypoint, (trueSpeed)); 
+	}
+
+}
+
---
+++ HunterEnableAttachedObjects.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterEnableAttachedObjects : MonoBehaviour {
+
+
+	public GameObject bow;
+
+	public GameObject leftHandArrow;
+
+	public GameObject rightHandArrow;
+
+	public GameObject failedArrow;
+
+	public GameObject availableObjects;
+
+	void Update() {
+		Destroy(availableObjects);
+	}
+
+	public void BowEnabled () {
+		bow.SetActive (true);
+	}
+
+	public void BowDisabled () {
+		bow.SetActive (false);
+	}
+
+	public void LeftHandArrowEnabled () {
+		leftHandArrow.SetActive (true);
+	}
+
+	public void LeftHandArrowDisabled () {
+		leftHandArrow.SetActive (false);
+	}
+
+	public void RightHandArrowEnabled () {
+		rightHandArrow.SetActive (true);
+	}
+
+	public void RightHandArrowDisabled () {
+		rightHandArrow.SetActive(false);
+	}
+
+	public void failedArrowEnabled () {
+		failedArrow.SetActive(true);
+	}
+
+}
