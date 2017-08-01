4265b5fb1d90934b59f03923edad2128411554cb
David Lacho <david.lacho@gmail.com>
2017-08-01 16:24:43 -0500

    Tsikwa Instantiate Objects when Swing
    Tsikwa instantiates objects when swings.

---
+++ TsikwaSwingObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TsikwaSwingObjects : MonoBehaviour {
+
+	public GameObject shawl;
+	public GameObject apron;
+	public GameObject paintBag;
+
+	public void throwShawl () {
+		Instantiate (shawl, transform.position, transform.rotation);
+	}
+
+	public void throwApron () {
+		Instantiate (apron, transform.position, transform.rotation);
+	}
+
+	public void throwPaintBag () {
+		Instantiate (paintBag, transform.position, transform.rotation);
+	}
+
+}
+
