680744e30f3e1618b6962eb9f00d63da6da41fb5
David Lacho <david.lacho@gmail.com>
2017-08-09 11:40:46 -0500

    Idle aim randomizer
    Created an idle animation randomizer so that default animation states
    cycle at random instead of in a looped sequence

---
+++ AllScenes/IdleAnimationRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAnimationRandomizer : MonoBehaviour {
+
+	void RandomizeIdleStance () {
+		Animator animator = GetComponent<Animator> ();
+		int randomNumber = Random.Range (1, 5);
+		animator.SetTrigger("Idle" + randomNumber);
+	}
+}
